fc176650481234ef3b9144e3bae4013bd40700c0
Kristina Weinberger <kweinberger@researchstudio.at>
2019-03-20 15:11:46 +0100

    Removed unused variable

--- webofneeds/won-bot/src/main/java/won/bot/app/GroupCycleBotApp.java
+++ webofneeds/won-bot/src/main/java/won/bot/app/GroupCycleBotApp.java
@@ -17,21 +17,19 @@
 package won.bot.app;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * App for EchoBot
  */
-public class GroupCycleBotApp
-{
+public class GroupCycleBotApp {
     public static void main(String[] args) throws Exception {
-        SpringApplication app = new SpringApplication(
-                new Object[]{"classpath:/spring/app/groupCycleBotApp.xml"}
-        );
+        SpringApplication app = new SpringApplication(new Object[] { "classpath:/spring/app/groupCycleBotApp.xml" });
         app.setWebEnvironment(false);
-        ConfigurableApplicationContext applicationContext =  app.run(args);
-        //Thread.sleep(5*60*1000);
-        //app.exit(applicationContext);
+        app.run(args);
+        // use for debugging purposes:
+        // ConfigurableApplicationContext applicationContext = app.run(args);
+        // Thread.sleep(5*60*1000);
+        // app.exit(applicationContext);
     }
 
 }
